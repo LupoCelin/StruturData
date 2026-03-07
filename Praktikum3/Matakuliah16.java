fdc24e63cf1657faba2bb05035eb0da2f5215c41
LupoCelin21 <emailanda@example.com>
2026-03-07 21:57:40 +0700

    Percobaan 3.4

---
+++ Praktikum3/Matakuliah16.java
@@ -0,0 +1,13 @@
+public class Matakuliah16 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int Jumlahjam;
+
+    public Matakuliah16 (String kode, String nama, int sks, int JumlahJam){
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.Jumlahjam = Jumlahjam;
+    }
+}
